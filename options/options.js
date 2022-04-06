5ab86fc3848f52e0993f3652336d31890440ca39
Crowdbotics <team@crowdbotics.com>
2022-04-06 18:52:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "hot_girl_tech_co_34631",
+  url: "https://hot_girl_tech_co_34631.botics.co",
+  api: "https://hot_girl_tech_co_34631.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
